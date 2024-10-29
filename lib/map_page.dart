05549423917ebccaff0a71583768c995d7dcabe9
amirhosein <amirhosein@Orcas-MacBook-Pro.local>
2024-10-29 14:38:46 +0330

    add flutter_map library and map_page.dart

---
+++ lib/map_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Map')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(35.6892, 51.3890), // Example coordinates
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+        ],
+      ),
+    );
+  }
+}
